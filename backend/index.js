faa9e577bc3be60fc0e15dae74224170d0fce261
Josechav12 <josechavarri12@gmail.com>
2025-10-28 23:06:42 -0300

    avanzamos en clase, enlazamos la BBDD con el back y conectamos con el front

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,22 @@
-//--- Require de las dependencias necesarias ---//
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config({path: '.env'});
-
-//--- Require de las rutas necesarias ---//
-const userRoutes = require('./src/routes/players.routes');
-
-//---------------------- Implementacion de Express e inyeccion de depencias/middlewares -----------------------//
+/*====================
+    Importaciones
+====================*/
+import express from "express";
 const app = express();
 
-//--- Implementacion de middlewares basicos de seguridad ---//
-app.use(helmet());
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(cors());
+import environments from "./src/api/config/environments.js";
 
-app.use((err, req, res, next) => 
-{
-    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {return res.status(400).json({ error: 'JSON inválido en la solicitud.' });}
-    next();
+const PORT = environments.port;
+
+
+/*==================
+    Endpoints
+==================*/
+app.get("/", (req, res) => {
+    res.send("Holis mundo desde Express.js");
 });
 
-//------------------------------- Endpoints -------------------------------//
-app.get('/', async (req, res) => {res.json({name: app.get('pkg').name, autor: app.get('pkg').author, descripcion: app.get('pkg').description, version: app.get('pkg').version,});});
 
-app.use('/api/usuarios', userRoutes);
-// app.use('/api/productos', playersRoutes);
-
-
-//--- Port defined ---//
-const PORT = process.env.port || 3102;
-app.listen(PORT, () => console.log(`API Backend corriendo en puerto ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
